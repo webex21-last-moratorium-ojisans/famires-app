5d4c876f26477fe5afa8f517a521f8689bcf936c
Yuji <yuji08752@gmail.com>
2024-03-04 01:27:52 +0900

    vite の追記

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+// vite.config.ts
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    remix({
+      ssr: false,
+    }),
+  ],
+});
